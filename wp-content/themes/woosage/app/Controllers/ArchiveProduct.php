27177544e7863995283e678068460f0f281620b1
Stefan Boshnakov <sboshnakov@gmail.com>
2018-12-30 14:20:18 +0200

    initial commit

---
+++ wp-content/themes/woosage/app/Controllers/ArchiveProduct.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+use Sober\Controller\Controller;
+
+class ArchiveProduct extends Controller
+{
+
+    function __construct()
+    {
+
+    }
+
+    public function test2()
+    {
+       // dd('Archive Product Page - Loop Products');
+    }
+}
---
+++ wp-content/themes/woosage/app/Controllers/SingleProduct.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+use Sober\Controller\Controller;
+
+class SingleProduct extends Controller
+{
+
+    function __construct()
+    {
+    }
+
+    public function test()
+    {
+       // dd('Single Product Page');
+    }
+}
