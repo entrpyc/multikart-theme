7b5941291e39f9dfeb428559ad00c3f82ce366dd
Stefan Bo <sboshnakov@gmail.com>
2019-02-08 09:45:17 +0200

    Woocommerce loop template fix

--- wp-content/themes/woosage/resources/views/woocommerce/loop/add-to-cart.blade.php
+++ wp-content/themes/woosage/resources/views/woocommerce/loop/add-to-cart.blade.php
@@ -17,10 +17,11 @@
 
 @php if ( !defined( 'ABSPATH' ) ) { exit; } @endphp
 
+@php
+	global $product;
+@endphp
 
-global $product;
-
-echo apply_filters( 'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
+{!! apply_filters( 'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
 	sprintf( '<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
 		esc_url( $product->add_to_cart_url() ),
 		esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
@@ -28,4 +29,7 @@ echo apply_filters( 'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
 		isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
 		esc_html( $product->add_to_cart_text() )
 	),
-$product, $args );
+$product, $args ) !!}
+
+
+
