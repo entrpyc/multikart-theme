ee523e114ca01f035acfa04664db1fc0fd1a5f3e
asen <asen@grind.bg>
2020-07-09 18:09:46 +0300

    integrated vegetables

--- wp-content/themes/woosage/app/Controllers/Vegetables.php
+++ wp-content/themes/woosage/app/Controllers/Vegetables.php
@@ -15,6 +15,9 @@ class Vegetables extends Controller
     {
         $data['id'] = get_the_ID();
 
+        $data['hero-slider'] = get_field('vegetables_hero_slider', $data['id']);
+        $data['parallax'] = get_field('vegetables_parallax', $data['id']);
+        $data['product-symbol'] = get_woocommerce_currency_symbol();
 
         return $data;
     }
@@ -67,4 +70,55 @@ class Vegetables extends Controller
      * All Post Categories Query
      * @return WP_Query
      */
+
+    public function products()
+    {
+        $query = new WP_Query($this->getQueryArgsProducts());
+        $products = [];
+
+        while ( $query->have_posts() ) : $query->the_post();
+            global $product;
+
+            $cats = [];
+            foreach (get_the_terms($product->ID, 'product_cat') as $cat) {
+                array_push($cats,  $cat->slug);
+            }
+
+            array_push($products, [
+                'href' => get_permalink(),
+                'image' => wp_get_attachment_url( $product->get_image_id() ),
+                'title' => get_the_title(),
+                'price' => $product->get_regular_price(),
+                'discount' => $product->get_sale_price(),
+                'cat' => $cats
+            ]);
+        endwhile;
+
+        wp_reset_query();
+
+        return $products;
+    }
+
+    /**
+     * Query args based on search and paged GET variables
+     * @return array
+     */
+    protected function getQueryArgsProducts()
+    {
+        $args = array(
+            'post_type'      => 'product',
+            'posts_per_page' => -1,
+        );
+
+        return $args;
+    }
+
+    public function product_cat()
+    {
+        $product_cat['new_product'] = 'new-product';
+        $product_cat['trending'] = 'trending';
+        $product_cat['special'] = 'special';
+
+        return $product_cat;
+    }
 }
