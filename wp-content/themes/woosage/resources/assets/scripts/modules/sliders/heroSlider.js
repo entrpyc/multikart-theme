bfdc32e2ea1b9a509eb449e97e90bc3fb559c748
unknown <georgi.g@grind.bg>
2020-07-24 17:37:23 +0300

    home sections

---
+++ wp-content/themes/woosage/resources/assets/scripts/modules/sliders/heroSlider.js
@@ -0,0 +1,15 @@
+import Swiper from 'swiper';
+export default function heroSlider() {
+    const sliderName = '.hero-slider';
+    if($(sliderName).length > 0) {        
+        var heroSlider = new Swiper(sliderName, {
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+        setTimeout(() => {
+            heroSlider.update();
+        }, 2000);
+    }
+}
---
+++ wp-content/themes/woosage/resources/assets/scripts/routes/vegetables.js
@@ -0,0 +1,7 @@
+import heroSlider from '../modules/sliders/heroSlider';
+export default {
+    init() {
+      heroSlider();
+      // JavaScript to be fired on the about us page
+    },
+};
