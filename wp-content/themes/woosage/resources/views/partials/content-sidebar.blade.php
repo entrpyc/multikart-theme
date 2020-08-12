f3a8f9de4dd950163912c7c2d69db9a55c32fb8d
asen <asen@grind.bg>
2020-08-12 14:20:51 +0300

    added inner product html and scss components

---
+++ wp-content/themes/woosage/resources/views/partials/content-sidebar.blade.php
@@ -0,0 +1,280 @@
+<div class="mass">
+  <div class="new-row">
+    <div class="col-grid sidebar">
+      <div class="sidebar">
+        <div class="collection-filter">
+          <div class="accordeon-module">
+            <div class="head">
+              <h3>brand</h3>
+            </div>
+            <div class="body">
+              <div class="fit">
+                <ul class="category-list">
+                  <li><a href="#">clothing</a></li>
+                  <li><a href="#">bags</a></li>
+                  <li><a href="#">footwear</a></li>
+                  <li><a href="#">watches</a></li>
+                  <li><a href="#">accessories</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="collection-banner gap">
+          <div class="banner">
+            <div class="icon-group flex">
+              <img src="" alt="">
+              <div class="text flex column">
+                <h4>Free Shipping</h4>
+                <p>Free Shipping World Wide</p>
+              </div>
+            </div>
+            <div class="icon-group flex">
+              <img src="" alt="">
+              <div class="text flex column">
+                <h4>24 X 7 Service</h4>
+                <p>Online Service For New Customer</p>
+              </div>
+            </div>
+            <div class="icon-group flex">
+              <img src="" alt="">
+              <div class="text flex column">
+                <h4>Festival Offer</h4>
+                <p>New Online Special Festival Offer</p>
+              </div>
+            </div>
+            <div class="icon-group flex">
+              <img src="" alt="">
+              <div class="text flex column">
+                <h4>Online Payment</h4>
+                <p>Contrary To Popular Belief.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="collection-featured-products splide gap">
+          <div class="splide__arrows">
+            <button class="splide__arrow splide__arrow--prev">
+              Prev
+            </button>
+            <button class="splide__arrow splide__arrow--next">
+              Next
+            </button>
+          </div>
+          <div class="top-bar">
+            <h5>NEW PRODUCT</h5>
+          </div>
+          <div class="splide__track">
+            <div class="slider flex splide__list">
+              <div class="page splide__slide">
+                <div class="product flex">
+                  <a href="">
+                    <img class="thumbnail" src="@asset('images/1.jpg')" alt="">
+                  </a>
+                  <div class="info flex column jc-center">
+                    <div class="stars"></div>
+                    <a href="" class="flex column">
+                      <h6>Slim Fit Cotton Shirt</h6>
+                      <h4>$500.00</h4>
+                    </a>
+                  </div>
+                </div>
+                <div class="product flex">
+                  <a href="">
+                    <img class="thumbnail" src="@asset('images/1.jpg')" alt="">
+                  </a>
+                  <div class="info flex column jc-center">
+                    <div class="stars"></div>
+                    <a href="" class="flex column">
+                      <h6>Slim Fit Cotton Shirt</h6>
+                      <h4>$500.00</h4>
+                    </a>
+                  </div>
+                </div>
+                <div class="product flex">
+                  <a href="">
+                    <img class="thumbnail" src="@asset('images/1.jpg')" alt="">
+                  </a>
+                  <div class="info flex column jc-center">
+                    <div class="stars"></div>
+                    <a href="" class="flex column">
+                      <h6>Slim Fit Cotton Shirt</h6>
+                      <h4>$500.00</h4>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="page splide__slide">
+                <div class="product flex">
+                  <a href="">
+                    <img class="thumbnail" src="@asset('images/1.jpg')" alt="">
+                  </a>
+                  <div class="info flex column jc-center">
+                    <div class="stars"></div>
+                    <a href="" class="flex column">
+                      <h6>Slim Fit Cotton Shirt</h6>
+                      <h4>$500.00</h4>
+                    </a>
+                  </div>
+                </div>
+                <div class="product flex">
+                  <a href="">
+                    <img class="thumbnail" src="@asset('images/1.jpg')" alt="">
+                  </a>
+                  <div class="info flex column jc-center">
+                    <div class="stars"></div>
+                    <a href="" class="flex column">
+                      <h6>Slim Fit Cotton Shirt</h6>
+                      <h4>$500.00</h4>
+                    </a>
+                  </div>
+                </div>
+                <div class="product flex">
+                  <a href="">
+                    <img class="thumbnail" src="@asset('images/1.jpg')" alt="">
+                  </a>
+                  <div class="info flex column jc-center">
+                    <div class="stars"></div>
+                    <a href="" class="flex column">
+                      <h6>Slim Fit Cotton Shirt</h6>
+                      <h4>$500.00</h4>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="page splide__slide">
+                <div class="product flex">
+                  <a href="">
+                    <img class="thumbnail" src="@asset('images/1.jpg')" alt="">
+                  </a>
+                  <div class="info flex column jc-center">
+                    <div class="stars"></div>
+                    <a href="" class="flex column">
+                      <h6>Slim Fit Cotton Shirt</h6>
+                      <h4>$500.00</h4>
+                    </a>
+                  </div>
+                </div>
+                <div class="product flex">
+                  <a href="">
+                    <img class="thumbnail" src="@asset('images/1.jpg')" alt="">
+                  </a>
+                  <div class="info flex column jc-center">
+                    <div class="stars"></div>
+                    <a href="" class="flex column">
+                      <h6>Slim Fit Cotton Shirt</h6>
+                      <h4>$500.00</h4>
+                    </a>
+                  </div>
+                </div>
+                <div class="product flex">
+                  <a href="">
+                    <img class="thumbnail" src="@asset('images/1.jpg')" alt="">
+                  </a>
+                  <div class="info flex column jc-center">
+                    <div class="stars"></div>
+                    <a href="" class="flex column">
+                      <h6>Slim Fit Cotton Shirt</h6>
+                      <h4>$500.00</h4>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="content">
+        <div class="product-display flex">
+          <div class="collection-gallery col-2 side-padding">
+            <div class="image-zoom">
+              <a href="@asset('images/36.jpg')">
+                <img src="@asset('images/36.jpg')" alt="">
+              </a>
+            </div>
+            <div class="images-list">
+    
+            </div>
+          </div>
+          <div class="collection-product-description col-2 side-padding">
+            <h2>Women Pink Shirt</h2>
+            <h4><del>$459.00</del><span>55% off</span></h4>
+            <h3>$32.96</h3>
+            <ul class="color-variant flex">
+                <li data-color="#d0edff"></li>
+                <li data-color="#f1e7e6"></li>
+                <li data-color="#bfbfbf"></li>
+            </ul>
+            <div class="product-description border-product">
+              <ul class="size-variant flex">
+                <li class="active"><a href="#">s</a></li>
+                <li><a href="#">m</a></li>
+                <li><a href="#">l</a></li>
+                <li><a href="#">xl</a></li>
+              </ul>
+                <h6 class="product-title">quantity</h6>
+                <div class="qty-box number">
+                  <div class="input-group">
+                    <span class="input-group-prepend">
+                      <button type="button" data-type="decrement"></button> 
+                    </span>
+                    <input type="number" name="quantity" value="1">
+                    <span class="input-group-prepend">
+                      <button type="button" data-type="increment"></button>
+                    </span>
+                  </div>
+                </div>
+            </div>
+            <div class="product-buttons"><a href="#" data-toggle="modal" data-target="#addtocart" class="btn btn-solid">add to cart</a> <a href="#" class="btn btn-solid">buy now</a></div>
+            <div class="border-product">
+                <h6 class="product-title">product details</h6>
+                <p>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem
+                    accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab
+                    illo inventore veritatis et quasi architecto beatae vitae dicta sunt,
+                    explicabo. Nemo enim ipsam voluptatem,</p>
+            </div>
+            <div class="border-product">
+                <h6 class="product-title">share it</h6>
+                <div class="collection-social-icon">
+                    <ul class="product-social">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                    </ul>
+                    <form class="d-inline-block">
+                        <button class="wishlist-btn"><i class="fa fa-heart"></i><span class="title-font">Add To WishList</span></button>
+                    </form>
+                </div>
+            </div>
+          </div>
+        </div>
+        <div class="tabs-module">
+          <div class="tab-navigation">
+            <ul>
+              <li>Description</li>
+              <li>DETAILS</li>
+              <li>VIDEO</li>
+              <li>WRITE REVIEW</li>
+            </ul>
+          </div>
+          <div class="tab-content">
+            <div class="tab-body">
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+            <div class="tab-body">
+              <p>Lry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+            <div class="tab-body">
+              <p>Lorem Ipsing and typesetting industry. Lorem Ipsndustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+            <div class="tab-body">
+              <p>Lm has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
