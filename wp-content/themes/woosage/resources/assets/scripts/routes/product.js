1bd070906ad3c1eae165319d0356b4fce0754b6f
asen <asen@grind.bg>
2020-08-13 11:24:45 +0300

    removed multikart files

--- wp-content/themes/woosage/resources/assets/scripts/routes/product.js
+++ wp-content/themes/woosage/resources/assets/scripts/routes/product.js
@@ -1,12 +1,3 @@
-import '../multikart/menu';
-import '../multikart/slick';
-import '../multikart/lazysizes.min';
-import '../multikart/popper.min';
-import '../multikart/bootstrap';
-import '../multikart/bootstrap-notify.min';
-import '../multikart/jquery.elevatezoom';
-import '../multikart/script';
-
 import Swiper from 'swiper';
 
  
